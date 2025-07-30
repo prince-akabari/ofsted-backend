b0bbe4697c7a539ed441666db230369f461c5afb
darshiljani <140292223+princemernstack@users.noreply.github.com>
2025-07-30 08:15:09 +0530

    Add sql

---
+++ sql/_AuditChecklist__202507292324.sql
@@ -0,0 +1,21 @@
+INSERT INTO public."AuditChecklist" (id,category,item,status,priority,"dueDate","assignedTo",evidence,"comments","createdAt","updatedAt") VALUES
+	 ('903341a9-716c-41cf-bdae-ea4154d4e6c6','Behavior Management','Beh Manag','incomplete'::public."AuditStatus",'medium'::public."PriorityLevel",'2025-07-30 18:30:00','405f38db-e669-4a04-b2d2-1a298e9c7753','{}','1. Positive Behaviour Support Plan
+2. Challenging Behaviour Incident Logs
+– Evidence of de-escalation strategies
+3. Staff Training Certificates (e.g. restraint training)
+4. Policy on Sanctions & Rewards','2025-07-25 01:49:58.224','2025-07-25 17:58:12.455'),
+	 ('b0776a8b-98e7-4b4f-892f-7b018aa0de7c','Health & Safety','Health and Safety Policy','incomplete'::public."AuditStatus",'low'::public."PriorityLevel",'2025-07-30 18:30:00','405f38db-e669-4a04-b2d2-1a298e9c7753','{}','1. Health and Safety Policy
+2. Risk Assessments (fire, trips, kitchen, etc.)
+3. Maintenance & Safety Check Logs
+– Fire alarms, PAT testing, etc.
+4. First Aid Training Certificates','2025-07-25 01:51:08.186','2025-07-25 17:58:12.455'),
+	 ('c0228d6b-98c7-4a41-8120-620a633aef71','Staff Recruitment','Safer Recruitment Policy','complete'::public."AuditStatus",'high'::public."PriorityLevel",'2025-07-31 18:30:00','405f38db-e669-4a04-b2d2-1a298e9c7753','{}','1. Safer Recruitment Policy
+2. Staff Vetting Checklist
+– DBS, references, right to work
+3. Interview & Induction Records','2025-07-25 01:50:38.143','2025-07-25 17:58:12.455'),
+	 ('e8ce537c-d17b-4c95-9f0b-f02db01f4810','Leadership and Management','Leadership and Management Policy','complete'::public."AuditStatus",'high'::public."PriorityLevel",'2025-08-28 18:30:00','405f38db-e669-4a04-b2d2-1a298e9c7753','{}','1. Organisational Structure Chart
+2. Quality Assurance Reports (e.g. Regulation 44 visits)
+3. Supervision & Appraisal Records
+4. Complaints Log
+5. Improvement Plan / Self-Assessment','2025-07-25 01:53:29.55','2025-07-25 17:58:12.455'),
+	 ('eb7cff9c-e827-48f1-8244-87c1c06afcc8','Safeguarding','Safeguarding Policy','complete'::public."AuditStatus",'low'::public."PriorityLevel",'2025-07-30 18:30:00','405f38db-e669-4a04-b2d2-1a298e9c7753','{1753374213196-Auto-debit-Registration-Acknowledgement.pdf}','Please Upload your evidance ','2025-07-24 03:29:35.764','2025-07-25 17:58:14.74');
